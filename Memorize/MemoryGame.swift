d5bae6798cfba2306ca7814a05652fc698583458
Calcasmy <technoatomic@gmail.com>
2021-02-19 13:21:17 -0500

    Initial Commit

---
+++ Memorize/MemoryGame.swift
@@ -0,0 +1,26 @@
+//
+//  MemoryGame.swift
+//  STOBTC
+//
+//  Created by Neptune on 2/18/21.
+//
+
+/*
+ *  This is the Model part of the MVVM (MODEL - VIEW - MODELVIEW) architecture for the Memory Game.
+ *
+ *
+ */
+
+import Foundation
+
+// Struct of the Model.
+// Does not have a : View and this is not a View part of the MVVM
+// The model can be a SQL database or Network thing or a class.
+struct MemoryGaem {
+    
+    var cards: Array<Card>
+    
+    struct Card {
+        
+    }
+}
